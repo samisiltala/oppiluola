6b757bd995d7566cd6d75de45b7fa265ef62c163
samisiltala <sami.siltala@gmail.com>
2026-03-05 16:46:45 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "oppiluola-cache-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
